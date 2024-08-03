cdf88c50cbe494143ce6391165fd8d65e0ee9798
Moisés <moisesavilar0@gmail.com>
2024-08-03 01:30:12 -0300

    Update UI/UX: Added autofocus property to 'nome' fields in forms

--- cria_lista/forms.py
+++ cria_lista/forms.py
@@ -6,21 +6,33 @@ class CadastraItensForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('nome', 'quantidade', 'valor',)
+        widgets = {
+            'nome': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
 
 
 class CriaListaForm(forms.ModelForm):
     class Meta:
         model = Lista
         fields = ('nome', 'tipo', 'categoria', 'meta_de_gastos',)
+        widgets = {
+            'nome': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
 
 
 class AtualizaNomeListaForm(forms.ModelForm):
     class Meta:
         model = Lista
         fields = ('nome', 'tipo', 'categoria', 'meta_de_gastos',)
+        widgets = {
+            'nome': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
 
 
 class EditarItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('nome', 'quantidade', 'valor',)
+        widgets = {
+            'nome': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
